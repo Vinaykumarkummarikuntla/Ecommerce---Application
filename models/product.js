cd7989149245db0f069be2c33870884df89a50b5
Vinaykumarkummarikuntla <kvk.9618@gmail.com>
2023-05-26 11:00:17 +0530

    sequelize added

--- models/product.js
+++ models/product.js
@@ -1,33 +1,59 @@
-const db = require('../util/database');
+const db = require("../util/database");
 
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
+// module.exports = class Product {
+//   constructor(id, title, imageUrl, description, price) {
+//     this.id = id;
+//     this.title = title;
+//     this.imageUrl = imageUrl;
+//     this.description = description;
+//     this.price = price;
+//   }
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+//   save() {
+//     return db.execute('INSERT INTO products(title,price,description,imageUrl) VALUES (?,?,?,?)',[this.title,this.price,this.description,this.imageUrl]);
+//   }
 
-  save() {
-    return db.execute('INSERT INTO products(title,price,description,imageUrl) VALUES (?,?,?,?)',[this.title,this.price,this.description,this.imageUrl]);
-  }
+//   static fetchAll() {
+//     return db.execute('SELECT * FROM products');
 
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-    
-  }
+//   }
 
-  static findById(id) {
+//   static findById(id) {
 
-    return db.execute('SELECT * FROM products WHERE id = ?', [id]);
-  }
+//     return db.execute('SELECT * FROM products WHERE id = ?', [id]);
+//   }
 
-  static deleteById(id){
-    return db.execute('DELETE FROM products WHERE id = ?', [id]);
-  }
-};
+//   static deleteById(id){
+//     return db.execute('DELETE FROM products WHERE id = ?', [id]);
+//   }
+// };
+
+const Sequelize = require("sequelize");
+const sequelize = require("../util/database");
+
+const Product = sequelize.define("products", {
+
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: true,
+    primaryKey: true,
+  },
+
+  title: Sequelize.STRING,
+
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,16 +1,32 @@
+// const fs = require('fs');
+// const mysql = require('mysql2');
+// const pool = mysql.createPool({
+//     host: 'ecommerce-mysql-database.mysql.database.azure.com',
+//     user: 'vinay',
+//     database: 'product',
+//     password: 'admin@9618',
+//     port: 3306,
+//     ssl: {
+//         ca: fs.readFileSync('/media/vinay/48248854248846C2/06-editing-the-product-data/certificate/DigiCertGlobalRootCA.crt.pem'),
+//         // sslCert: fs.readFileSync('client-cert.pem'), // Path to the client certificate file (if applicable)
+//         // sslKey: fs.readFileSync('client-key.pem') // Path to the client private key file (if applicable)
+//     }
+// });
+
+// module.exports = pool.promise();
+
+
 const fs = require('fs');
-const mysql = require('mysql2');
-const pool = mysql.createPool({
-    host: 'ecommerce-mysql-database.mysql.database.azure.com',
-    user: 'vinay',
-    database: 'product',
-    password: 'admin@9618',
-    port: 3306,
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('product', 'vinay', 'admin@9618', {
+  dialect: 'mysql',
+  host: 'ecommerce-mysql-database.mysql.database.azure.com',
+  dialectOptions: {
     ssl: {
-        ca: fs.readFileSync('/media/vinay/48248854248846C2/06-editing-the-product-data/certificate/DigiCertGlobalRootCA.crt.pem'),
-        // sslCert: fs.readFileSync('client-cert.pem'), // Path to the client certificate file (if applicable)
-        // sslKey: fs.readFileSync('client-key.pem') // Path to the client private key file (if applicable)
+      ca: fs.readFileSync('/media/vinay/48248854248846C2/ShopEasy/certificate/DigiCertGlobalRootCA.crt.pem'), // Path to the CA certificate
+      
     }
+  }
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
